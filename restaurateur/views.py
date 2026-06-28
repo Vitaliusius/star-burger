aaa47ac67ccf0e7c47bb1a445fce336fcd6daf9f
Vitaliy <vitalianobro@gmail.com>
2026-06-28 15:30:16 +0300

    added exception catching

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -19,20 +19,23 @@ from foodcartapp.models import (
 
 
 def fetch_coordinates(address):
-    base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": settings.YANDEX_API,
-        "format": "json",
-    })
-    response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+    try:
+        base_url = "https://geocode-maps.yandex.ru/1.x"
+        response = requests.get(base_url, params={
+            "geocode": address,
+            "apikey": settings.YANDEX_API,
+            "format": "json",
+        })
+        response.raise_for_status()
+        found_places = response.json()['response']['GeoObjectCollection']['featureMember']
 
-    if not found_places:
+        if not found_places:
+            return None
+
+        most_relevant = found_places[0]
+        lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    except requests.exceptions.HTTPError:
         return None
-
-    most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
 
     return lat, lon
 
