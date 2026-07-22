6f6f4950be8a3a49b50f5ece4922a094d4372be7
Vitaliy <vitalianobro@gmail.com>
2026-07-22 13:29:52 +0300

    add prod version

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,34 +1,19 @@
 #!/bin/bash
-
 set -e
-echo "Обновление кода репозитория"
-git pull
 
-echo "Обновление зависимостей..."
-cd /opt/star-burger/
-source venv/bin/activate
-pip install -r requirements.txt
+echo " Pulling changes from Git "
+git pull origin main
 
-echo "Сборка фронтенда"
-cd /opt/star-burger/
-npm ci --dev
-./node_modules/.bin/parcel watch bundles-src/index.js --dist-dir bundles --public-url="./"
+echo " Building and starting Docker containers "
+docker compose -f docker-compose.prod.yaml up -d --build
 
-echo "Сборка статики..."
-./venv/bin/python manage.py collectstatic --noinput
+echo " Running Django Migrations "
+docker compose -f docker-compose.prod.yaml exec -T backend python manage.py migrate --no-input
 
-echo "Применение миграций..."
-./venv/bin/python manage.py migrate --noinput
+echo " Collecting Static Files "
+docker compose -f docker-compose.prod.yaml exec -T backend python manage.py collectstatic --no-input
 
-echo "Перезапуск Systemd"
-sudo systemctl restart starburger
+echo " Reloading Nginx "
 sudo systemctl reload nginx
 
-echo "Деплой успешно завершен"
-echo "Уведомление Rollbar о новом деплое..."
-
-curl https://api.rollbar.com/api/1/deploy \
-  -H "X-Rollbar-Access-Token: 9654fff7642249c0ace4d1d87c7ddc8f" \
-  -H "Content-Type: application/json" \
-  -X POST \
-  -d '{"environment": "production", "revision": "'$(git rev-parse HEAD)'", "local_username": "'$(whoami)'", "comment": "Автоматический деплой"}'
+echo " Deployment finished "
