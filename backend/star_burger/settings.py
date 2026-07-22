f4874f75c4497bd777cad1eaf969d4c25cba7762
Vitaliy <vitalianobro@gmail.com>
2026-07-22 17:36:05 +0300

    add CSRF_TRUSTED_ORIGINS

--- backend/star_burger/settings.py
+++ backend/star_burger/settings.py
@@ -26,6 +26,8 @@ DEBUG = env.bool('DEBUG', False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
+
 ROLLBAR = {
     'access_token': ROLLBAR_ACCESS_TOKEN,
     'environment': ROLLBAR_ENVIRONMENT,
